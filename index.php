471f032bc9b7054b2ffb494879fb387f6d28ffa1
Fernada1305 <101894024+Fernada1305@users.noreply.github.com>
2022-03-24 16:36:39 -0500

    20220324 PRACTICA PHP (1)

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+   
+</head>
+<body>
+    <h1>PRÁCTICA PHP</h1>
+    <?php
+    echo "Vamos a realizar operaciones";
+    echo "<br>"; //salto de línea
+    echo 3+3;
+    ?>
+</body>
+</html>
