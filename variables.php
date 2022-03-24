3028c53436f3ca3899ce1f3ee2a3b0b082147948
Fernada1305 <101894024+Fernada1305@users.noreply.github.com>
2022-03-24 17:03:07 -0500

    20220324 PRÁCTICA PHP (2)

---
+++ variables.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+   
+</head>
+<body>
+    <h1>DEFINIR VARIABLES</h1>
+    <?php
+//Definir variable como texto
+$miVariable = "Hola variable";
+//Variable en explorador
+echo $miVariable;
+//Asignar valor
+echo "<br>";
+$miValor = 2;
+//Operaciones
+echo 3*$miValor;
+?>
+</body>
+</html>
